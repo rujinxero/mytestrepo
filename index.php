51bd20888a99b53ebb6741febaf9f4d0c51bbb17
Gabriel Tumbaga <gtumbaga@decisioncousnel.com>
2014-04-09 10:39:30 -0700

    made HTML5

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
 <?php
+$poop = "bewbs";
 print "hello hello!! and more stuff yay";
+print "Gabe was here...";
+
 ?>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
