f9db0e08dfdd692b827a0ef82321b7f4ac6cfd35
Gabriel Tumbaga <gtumbaga@decisioncousnel.com>
2014-04-10 17:44:11 -0700

    made class example

--- index.php
+++ index.php
@@ -1,16 +1,75 @@
 <?php
-print "hello hello!! and more stuff yay";
-print "gabe was here...";
-?>
-=======
-<!DOCTYPE html>
+ 
+    class Human {
+        public $hair = "black";
+        
+        
+        //Called only when the this class is used to create a new object
+        public function __construct()
+        {
+            echo 'The class "', __CLASS__, '" was initiated!<br />';
+        }
+        
+        public function __destruct()
+        {
+            echo 'The class "', __CLASS__, '" was destroyed.<br />';
+        }
+        
+        public function setHairColor($newval)
+        {
+            $this->hair = $newval;
+        }
+     
+        public function getHairColor()
+        {
+            return $this->hair . "<br />";
+        }
+        
+        
+    }
+    
+    class Male extends Human
+    {
+        public $sex = "M";
+        //Called when getting destroyed...
+        public function __destruct()
+        {
+            parent::__destruct(); //Call the parent class's destruct 1st
+            echo 'The class "', __CLASS__, '" was destroyed.<br />';
+        }
+    }
+    
+    class Female extends Human
+    {
+        public $sex = "F";
+        //Called when getting destroyed...
+        public function __destruct()
+        {
+            parent::__destruct(); //Call the parent class's destruct 1st
+            echo 'The class "', __CLASS__, '" was destroyed.<br />';
+        }
+    }
+    
+     
+    $boy = new Male;
+    $girl = new Female;
+    
+    
+        
+    
+ 
+?><!DOCTYPE html>
 <html>
 <head>
 <title>Title of the document</title>
 </head>
 
 <body>
-The content of the document......
+    <?php
+        $boy->setHairColor("green");
+        echo $boy->getHairColor();
+        echo $girl->sex . "<br/>";
+    ?>
 </body>
 
 </html>
